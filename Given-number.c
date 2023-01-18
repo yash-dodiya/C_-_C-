8657cde302fd21ca4b781d53d01e04218b11721b
yash dodiya <yashsinhrajput1@gmail.com>
2023-01-18 11:25:41 +0530

    Given_num

---
+++ Given-number.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+/*Write a program to find the Max number from the given three number using 
+Nested If
+*/
+
+main()
+{
+	int nu1,nu2,nu3;
+	
+	printf("\n enter number 1:");
+	scanf("%d",&nu1);
+	
+	printf("\n enter number 2:");
+	scanf("%d",&nu2);
+	
+	printf("\n enter number 3:");
+	scanf("%d",&nu3);
+	
+	if(nu1>nu2 && nu1>nu3)
+	{
+		printf("\n number 1 is max number:");
+	}
+	else if(nu2>nu3)
+		{
+			printf("\n number 3 is max number:");
+		}
+	}
+	else 
+	{
+	
+		printf("\n number 2 is given number:");
+		
+		
+      }
+}
